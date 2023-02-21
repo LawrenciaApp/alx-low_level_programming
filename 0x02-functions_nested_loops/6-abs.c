d3946398f4e197321438d3804581e773ecc04ffa
LawrenciaApp <appiahlawrencia69@gmail.com>
2023-02-21 14:29:56 -0800

    hfg

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign - functions for lowercase checks
+ * *n: char to be checked
+ * Return 1 for lowercase  and 0 for anything else
+ */
+int _abs(int n)
+{
+if (n > 0)
+{
+return (n);
+}
+if (n == 0)
+{
+return (n);
+}
+if (n < 0)
+{
+n = n * -i;
+return (n);
+}
+return (0);
+}
