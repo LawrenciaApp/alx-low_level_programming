248e446b516a428ff252bf98833fcb7fce849c48
LawrenciaApp <appiahlawrencia69@gmail.com>
2023-02-24 00:41:24 -0800

    jojo

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,26 +5,4 @@
  * print_diagonal - check the code
  *
  * @n: param
- * Return: Always 0.
- */
-void print_diagonal(int n)
-{
-	if (n < 1)
-	{
-		putchar('\n');
-	}
-	else
-	{
-		int i, j;
-
-		for (i = 0; i < n; i++)
-		{
-			for (j = 0; j < i; j++)
-			{
-				putchar(' ');
-			}
-			putchar('\\');
-			putchar('\n');
-		}
-	}
-}
+ * Return: Al}
