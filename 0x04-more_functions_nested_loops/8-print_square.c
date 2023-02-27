1e1eec99a942ad2859a34c15638fcfb434b868ab
LawrenciaApp <appiahlawrencia69@gmail.com>
2023-02-27 14:17:49 -0800

    long

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - check the code
+ *
+ * @size: param
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+	if (size < 1)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
