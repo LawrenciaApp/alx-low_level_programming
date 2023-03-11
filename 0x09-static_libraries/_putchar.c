dfc8b3fb6cf7b61cabe6d023fd6a9a2adcd847ef
LawrenciaApp <appiahlawrencia6@gmail.com>
2023-03-11 01:41:05 -0800

    pls

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <unistd.h>
+
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -11,7 +12,3 @@ int _putchar(char c)
 {
  return (write(1, &c, 1));
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> b10fbdf933643d83c4a84615ddcc87f79cd1e2bf
