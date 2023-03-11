d57dd182081404d06ff1ed2dee33c20ecd586612
LawrenciaApp <appiahlawrencia6@gmail.com>
2023-03-11 01:06:53 -0800

    dne

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - check the code
+ *
+ * @s: param1
+ * @c: param2
+ * Return: pointer.
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c)
+		{
+			return (&s[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
