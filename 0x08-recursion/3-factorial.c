9e9839de1bfb8265c1bac972ac00f3eb05999253
LawrenciaApp <appiahlawrencia69@gmail.com>
2023-06-07 10:01:02 -0700

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,21 +1,23 @@
 #include "main.h"
 
 /**
- * _pow_recursion - check the code
+ * factorial - check the code
  *
- * @x: param1
- * @y: param2
- * Return: pow.
+ * @n: param2
+ * Return: factorial.
  */
-int _pow_recursion(int x, int y)
+int factorial(int n)
 {
-	if (y == 0)
+	if (n >= 1)
+	{
+		return (n * factorial(n - 1));
+	}
+	else if (n == 0)
 	{
 		return (1);
 	}
-	if (y < 0)
+	else
 	{
 		return (-1);
 	}
-	return (x * _pow_recursion(x, y - 1));
-}
+}}
