8e2337444cce377517a2e1054e81d912a1c5851d
LawrenciaApp <appiahlawrencia69@gmail.com>
2023-06-30 02:25:01 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,27 @@
 #include "lists.h"
 
 /**
- * print_listint - ...
+ * print_list - ...
  *
  * @h: ...
  * Return: ...
  */
-size_t print_listint(const listint_t *h)
+size_t print_list(const list_t *h)
 {
-const listint_t *temp = h;
-size_t count = 0;
+	size_t i = 0;
 
-if (h == NULL)
-return (0);
-while (temp != NULL)
-{
-printf("%d\n", temp->n);
-count++;
-temp = temp->next;
-}
-return (count);
+	if (h == NULL)
+	{
+		return (0);
+	}
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
