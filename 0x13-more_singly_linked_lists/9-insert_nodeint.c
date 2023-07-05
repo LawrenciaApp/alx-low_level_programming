695f1b38ac7d8b0494221ac3abd953b788cac0d6
LawrenciaApp <appiahlawrencia69@gmail.com>
2023-07-05 07:42:34 -0700

    node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,35 +1,56 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - ...
+ * insert_nodeint_at_index - insert
+ * @head: pointer to first listint_t node
+ * @idx: index to insert at
+ * @n: int n of the new node
  *
- * @head: ...
- * @idx: ...
- * @n: ...
- * Return: ...
+ * Description: remove head node of list
+ *
+ * Return: int
  */
-listint_t *insert_nodeint_at_index(listint_t **head,
-unsigned int idx, int n)
+listint_t *insert_nodeint_at_index(
+	listint_t **head, unsigned int idx, int n
+)
 {
-listint_t *newNode;
-listint_t *temp = *head;
-unsigned int pos = 0;
+	listint_t *tmp, *newnode;
+	unsigned int i = -1;
 
+	tmp = *head;
+	newnode = malloc(sizeof(listint_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = n;
+	newnode->next = NULL;
+	while (i != idx)
+	{
+		i++;
+		if (!tmp && i == 0 && i == idx)
+		{
+			*head = newnode;
+			return (newnode);
+		}
+		if (tmp && i == 0 && i == idx)
+		{
+			newnode->next = tmp;
+			*head = newnode;
+			return (newnode);
+		}
+		if (i + 1 == idx)
+		{
+			newnode->next = tmp->next;
+			tmp->next = newnode;
+			return (newnode);
+		}
+		if (tmp == NULL)
+		{
+			free(newnode);
+			return (NULL);
+		}
+		tmp = tmp->next;
+	}
 
-
-newNode = (listint_t *) malloc(sizeof(listint_t));
-if (newNode == NULL)
-{
-return (NULL);
-}
-
-while (pos < idx - 1)
-{
-temp = temp->next;
-pos++;
-}
-newNode->n = n;
-newNode->next = temp->next;
-temp->next = newNode;
-return (temp->next);
+	free(newnode);
+	return (NULL);
 }
