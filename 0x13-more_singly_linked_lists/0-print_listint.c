9e01053d92598d4a5dbe9c21a2b85e770911d816
LawrenciaApp <appiahlawrencia69@gmail.com>
2023-07-05 07:30:25 -0700

    print

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,23 +1,25 @@
-i#include "lists.h"
+#include "lists.h"
 
 /**
- * print_listint - ...
+ * print_listint - print lists
+ * @h: listint linked list
  *
- * @h: ...
- * Return: ...
+ * Description: print the int n member of the each
+ * element in the lists
+ *
+ * Return: size_t
  */
+
 size_t print_listint(const listint_t *h)
 {
-const listint_t *temp = h;
-size_t count = 0;
+	size_t elements = 0;
 
-if (h == NULL)
-return (0);
-while (temp != NULL)
-{
-printf("%d\n", temp->n);
-count++;
-temp = temp->next;
-}
-return (count);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		elements++;
+	}
+
+	return (elements);
 }
